f4c816105be65a3533cd2145dc75127deea5c262
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-06-03 14:36:42 +0200

    Crypto functions

--- app/src/main/java/com/webwemser/classifiedapp/RegisterActivity.java
+++ app/src/main/java/com/webwemser/classifiedapp/RegisterActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import org.spongycastle.crypto.digests.SHA256Digest;
+import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
+
 import java.security.SecureRandom;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKeyFactory;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,7 +25,18 @@ public class RegisterActivity extends AppCompatActivity {
         EditText userName = (EditText)findViewById(R.id.username);
         if(!userName.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
             //Username and Password are present
-           
+
+
+        }
+    }
+    protected void register(String userName, String password) {
+        if(!userName.isEmpty() && !password.isEmpty()) {
+
+            SecureRandom random = new SecureRandom();
+            //The random bytes
+            byte[]   bytes=  random.generateSeed(64);
+            PKCS5S2ParametersGenerator generator = new PKCS5S2ParametersGenerator(new SHA256Digest());
+            
 
         }
     }
