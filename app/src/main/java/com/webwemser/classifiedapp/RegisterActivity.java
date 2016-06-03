00520291fe740be41cbab44e1a904203e7a17978
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-06-03 14:15:16 +0200

    Reigster

--- app/src/main/java/com/webwemser/classifiedapp/RegisterActivity.java
+++ app/src/main/java/com/webwemser/classifiedapp/RegisterActivity.java
@@ -2,6 +2,12 @@ package com.webwemser.classifiedapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.security.SecureRandom;
+
+import javax.crypto.KeyGenerator;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void register(View view) {
+        EditText password   = (EditText)findViewById(R.id.password);
+        EditText userName = (EditText)findViewById(R.id.username);
+        if(!userName.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+            //Username and Password are present
+           
+
+        }
     }
 }
