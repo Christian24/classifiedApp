9fae5f32bec3421adff963f12f8ec82100d2526b
Jannik92 <jannikwortmann92@gmail.com>
2016-06-03 15:10:11 +0200

    Chat layout template

--- app/src/main/java/com/webwemser/classifiedapp/ChatsActitvity.java
+++ app/src/main/java/com/webwemser/classifiedapp/ChatsActitvity.java
@@ -5,17 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class ChatsActitvity extends AppCompatActivity {
 
     public static final String USER = "USERNAME";
     private EditText username;
+    private ScrollView scroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats_actitvity);
         username = (EditText)findViewById(R.id.username);
+        scroll = (ScrollView)findViewById(R.id.chat_scroll);
+        this.setTitle("Chats");
     }
 
     public void startChat(View view) {
