8ce0d7fa039c91e0e1c4a9453b6ece53e0d1640e
Jannik92 <jannikwortmann92@gmail.com>
2016-06-03 14:36:06 +0200

    Username on Chattitle

--- app/src/main/java/com/webwemser/classifiedapp/ChatsActitvity.java
+++ app/src/main/java/com/webwemser/classifiedapp/ChatsActitvity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ChatsActitvity extends AppCompatActivity {
+
+    public static final String USER = "USERNAME";
+    private EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats_actitvity);
+        username = (EditText)findViewById(R.id.username);
     }
+
     public void startChat(View view) {
         Intent intent = new Intent(this,SendActivity.class);
+        intent.putExtra(USER,username.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/webwemser/classifiedapp/SendActivity.java
+++ app/src/main/java/com/webwemser/classifiedapp/SendActivity.java
@@ -1,5 +1,6 @@
 package com.webwemser.classifiedapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(ChatsActitvity.USER);
+        this.setTitle(username);
     }
 }
